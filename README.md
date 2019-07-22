f4c38a7f7b82912e1ac437726dff2a1a9f7b4387
diwal123 <diwalpyakurel@gmail.com>
2019-07-21 19:21:57 -0500

    Desc for read Me

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Scrapper
+
+This web application scraps the news from the BBC official websites. Beside scrapping the news, it also allows the user to comment/ make notes on the articles. The application uses MOngo DB as the database.
+
+![](blank.JPG)
+
+##Technologies Used
+Axios
+Cheerio
+MongoDB
+Node/Express
+JavaScript/ jQuery
+HTML
+CSS
+
+
+##Deployed Version
+[BBC Scrapper](https://polar-thicket-41813.herokuapp.com/)
+
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var cheerio = require("cheerio");
 
 var db = require("./models");
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8000;
 
 var app = express();
 
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapper";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrappessr";
 
 mongoose.connect(MONGODB_URI);
 
